cdd643608b10313a3d46b61f4e3191fc9db70090
Panwar <harsh.panwar@capgemini.com>
2021-05-13 23:54:56 +0530

    My Commit 0n 13th Night

---
+++ src/main/java/com/harsh/apigatewaydemo1/ApiGatewayDemo1Application.java
@@ -0,0 +1,19 @@
+package com.harsh.apigatewaydemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableSwagger2
+public class ApiGatewayDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayDemo1Application.class, args);
+	}
+
+}
